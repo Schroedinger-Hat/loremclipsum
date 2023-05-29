12c75ca54d9be0d25fae8707ed9db60e1f581ae9
404answernotfound <404answernotfound.public@gmail.com>
2023-05-29 21:09:10 +0200

    releasing new version and formula

--- Formula/loremclipsum.rb
+++ Formula/loremclipsum.rb
@@ -2,7 +2,7 @@ class Loremclipsum < Formula
     desc "A CLI tool to copy-paste lorem ipsum text in different flavors."
     homepage "https://github.com/Schrodinger-Hat/loremclipsum"
     url "https://github.com/Schrodinger-Hat/loremclipsum/raw/main/loremclipsum.tar.gz"
-    sha256 "83e57ff423a36eb961fda2c073f65ed0edfa96c6755a4311b03d911cad6607c8"
+    sha256 "a2fe4faaecf04e1f718a3e1f08bc619fbe54a1307c34b96495aa2c62afda5f2a"
     version "1.0.1"
     license "MIT"
   
